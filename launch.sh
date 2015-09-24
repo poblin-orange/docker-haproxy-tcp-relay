15b1b2a5ca164c0ec523b0e10dcad3a3aa116f8e
Pierre Oblin <pierre.oblin@orange.com>
2015-09-24 22:39:57 +0200

    adapt config file to launch haproxy. listen port is 8080 always inside the container. fix expose port fix CMD to launch.sh

--- launch.sh
+++ launch.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
+
+#resolv vars in template file to haproxy config file
 cp /usr/local/etc/haproxy/haproxy.cfg.template /usr/local/etc/haproxy/haproxy.cfg
 sed -i -e 's/BACKEND_PORT1/$BACKEND_PORT1/g' /usr/local/etc/haproxy/haproxy.cfg
 sed -i -e 's/BACKEND_PORT2/$BACKEND_PORT2/g' /usr/local/etc/haproxy/haproxy.cfg
 sed -i -e 's/BACKEND1/$BACKEND1/g' /usr/local/etc/haproxy/haproxy.cfg
 sed -i -e 's/BACKEND2/$BACKEND2/g' /usr/local/etc/haproxy/haproxy.cfg
-sed -i -e 's/RELAY_PORT/$RELAY_PORT/g' /usr/local/etc/haproxy/haproxy.cfg
 
+
+#check config
 cat /usr/local/etc/haproxy/haproxy.cfg
+
+#launch haproxy with generated config
+haproxy -f /usr/local/etc/haproxy/haproxy.cfg
