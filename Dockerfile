a0b205ec1464a780f59c78f24b2c3b6bc14a0b83
Pierre Oblin <pierre.oblin@orange.com>
2015-09-24 13:44:31 +0200

    add templating mechanism to haproxy for 2 backends

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,8 @@
 FROM haproxy:1.5
 MAINTAINER Orange Elpaaso Team <xx@orange.com>
-COPY haproxy.cfg /usr/local/etc/haproxy/haproxy.cfg
+COPY haproxy.cfg.template /usr/local/etc/haproxy/haproxy.cfg.template
+COPY configure.sh /usr/local/etc/haproxy/configure.sh
+
+RUN chmod ugo+x configure.sh
+
+CMD ["haproxy", "-f", "/usr/local/etc/haproxy/haproxy.cfg"]
---
+++ configure.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cp /usr/local/etc/haproxy/haproxy.cfg.template /usr/local/etc/haproxy/haproxy.cfg
+sed -i -e 's/BACKEND_PORT1/$BACKEND_PORT1/g' /usr/local/etc/haproxy/haproxy.cfg
+sed -i -e 's/BACKEND_PORT2/$BACKEND_PORT2/g' /usr/local/etc/haproxy/haproxy.cfg
+sed -i -e 's/BACKEND1/$BACKEND1/g' /usr/local/etc/haproxy/haproxy.cfg
+sed -i -e 's/BACKEND2/$BACKEND2/g' /usr/local/etc/haproxy/haproxy.cfg
+cat /usr/local/etc/haproxy/haproxy.cfg
