e1cbd2628b44ae7de827ca85e4f363ffa4ba74d7
Pierre Oblin <pierre.oblin@orange.com>
2015-09-24 22:00:22 +0200

    add RELAY_PORT configution variable

--- configure.sh
+++ configure.sh
@@ -4,4 +4,6 @@ sed -i -e 's/BACKEND_PORT1/$BACKEND_PORT1/g' /usr/local/etc/haproxy/haproxy.cfg
 sed -i -e 's/BACKEND_PORT2/$BACKEND_PORT2/g' /usr/local/etc/haproxy/haproxy.cfg
 sed -i -e 's/BACKEND1/$BACKEND1/g' /usr/local/etc/haproxy/haproxy.cfg
 sed -i -e 's/BACKEND2/$BACKEND2/g' /usr/local/etc/haproxy/haproxy.cfg
+sed -i -e 's/RELAY_PORT/$RELAY_PORT/g' /usr/local/etc/haproxy/haproxy.cfg
+
 cat /usr/local/etc/haproxy/haproxy.cfg
