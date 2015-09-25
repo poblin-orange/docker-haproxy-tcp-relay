8f1b86fc3454fbef0899e8bd6dc9e21196f8975d
Pierre Oblin <pierre.oblin@orange.com>
2015-09-26 01:56:35 +0200

    FIX: escaping env var

--- launch.sh
+++ launch.sh
@@ -2,11 +2,10 @@
 
 #resolv vars in template file to haproxy config file
 cp /usr/local/etc/haproxy/haproxy.cfg.template /usr/local/etc/haproxy/haproxy.cfg
-sed -i -e 's/BACKEND_PORT1/$BACKEND_PORT1/g' /usr/local/etc/haproxy/haproxy.cfg
-sed -i -e 's/BACKEND_PORT2/$BACKEND_PORT2/g' /usr/local/etc/haproxy/haproxy.cfg
-sed -i -e 's/BACKEND1/$BACKEND1/g' /usr/local/etc/haproxy/haproxy.cfg
-sed -i -e 's/BACKEND2/$BACKEND2/g' /usr/local/etc/haproxy/haproxy.cfg
-
+sed -i -e 's/BACKEND_PORT1/'$BACKEND_PORT1'/g' /usr/local/etc/haproxy/haproxy.cfg
+sed -i -e 's/BACKEND_PORT2/'$BACKEND_PORT2'/g' /usr/local/etc/haproxy/haproxy.cfg
+sed -i -e 's/BACKEND1/'$BACKEND1'/g' /usr/local/etc/haproxy/haproxy.cfg
+sed -i -e 's/BACKEND2/'$BACKEND2'/g' /usr/local/etc/haproxy/haproxy.cfg
 
 #check config
 cat /usr/local/etc/haproxy/haproxy.cfg
